0c7cf8826860457e513f86ac6b59ac8d0148f34f
subodhghonge <96947438+subodhghonge@users.noreply.github.com>
2024-01-06 20:46:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+  navigation.classList.toggle("show");
+});
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
